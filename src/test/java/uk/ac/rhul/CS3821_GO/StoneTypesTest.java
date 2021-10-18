2c48cf03ab32774701b8211f2ce3149e36e1f2be
Jai Gautam <jaigautam1405@gmail.com>
2021-10-18 03:40:36 +0100

    Created pom + test suites and enum for Go Stone Types

---
+++ src/test/java/uk/ac/rhul/CS3821_GO/StoneTypesTest.java
@@ -0,0 +1,33 @@
+package uk.ac.rhul.CS3821_GO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+class StoneTypesTest {
+
+    @BeforeEach
+    void setUp() {
+        
+    }
+
+    @Test
+    void testCardinality(){
+        assertEquals(3,StoneTypes.values().length);
+    }
+
+
+    @Test
+    void testPlacedStones(){
+        boolean containsWhite = Arrays.stream(StoneTypes.values()).anyMatch(StoneTypes.WHITE::equals);
+        assertTrue(containsWhite);
+        boolean containsBlack = Arrays.stream(StoneTypes.values()).anyMatch(StoneTypes.BLACK::equals);
+        assertTrue(containsBlack);
+
+    }
+
+    @Test
+    void testNoStonePlaced(){
+        boolean containsNone = Arrays.stream(StoneTypes.values()).anyMatch(StoneTypes.NONE::equals);
+        assertTrue(containsNone);
+    }
+}
