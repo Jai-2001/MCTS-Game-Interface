4e43c6ba1a2b15553cdd7e0cce6a49c18f10a9ad
Jai Gautam <jaigautam1405@gmail.com>
2021-10-19 00:31:01 +0100

    Created fields and method logic to switch players each turn.

--- src/main/java/uk/ac/rhul/CS3821_GO/GoModel.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/GoModel.java
@@ -2,11 +2,18 @@ package uk.ac.rhul.CS3821_GO;
 
 public class GoModel {
 
+    private TurnState currentPlayerTurn;
+
+    GoModel(){
+        this.currentPlayerTurn = new TurnState();
+    }
+
     public TurnState getCurrentTurn() {
-        return new TurnState();
+        return this.currentPlayerTurn;
     }
 
     public void nextTurn() {
+        this.currentPlayerTurn.changePlayer();
     }
 }
 
