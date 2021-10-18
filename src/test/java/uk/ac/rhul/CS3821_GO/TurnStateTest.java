57d5e76faa6f15a9478cf959ff5b64f761763a81
Jai Gautam <jaigautam1405@gmail.com>
2021-10-19 00:11:38 +0100

    Created tests for mass and arbitrary numbers of turn switches

--- src/test/java/uk/ac/rhul/CS3821_GO/TurnStateTest.java
+++ src/test/java/uk/ac/rhul/CS3821_GO/TurnStateTest.java
@@ -3,6 +3,8 @@ package uk.ac.rhul.CS3821_GO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TurnStateTest {
@@ -23,8 +25,36 @@ class TurnStateTest {
         StoneTypes whiteStone = defaultStates.getCurrentPlayer().getType();
         assertEquals(StoneTypes.WHITE, whiteStone);
         defaultStates.changePlayer();
-        StoneTypes currentStone = defaultStates.getCurrentPlayer().getType();
-        assertEquals(StoneTypes.WHITE, currentStone);
+        StoneTypes blackStone = defaultStates.getCurrentPlayer().getType();
+        assertEquals(StoneTypes.BLACK, blackStone);
     }
 
+    @Test
+    void testMassSwitches(){
+        StoneTypes current;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            current = defaultStates.getCurrentPlayer().getType();
+            if(i%2 == 0){
+                assertEquals(StoneTypes.WHITE, current);
+            }  else {
+                assertEquals(StoneTypes.BLACK, current);
+            }
+            defaultStates.changePlayer();
+        }
+    }
+
+    @Test
+    void testArbitrarySwitches(){
+        StoneTypes current;
+        Random rng = new Random();
+        for (int i = 0; i < rng.nextInt(); i++) {
+            current = defaultStates.getCurrentPlayer().getType();
+            if(i%2 == 0){
+                assertEquals(StoneTypes.WHITE, current);
+            }  else {
+                assertEquals(StoneTypes.BLACK, current);
+            }
+            defaultStates.changePlayer();
+        }
+    }
 }
