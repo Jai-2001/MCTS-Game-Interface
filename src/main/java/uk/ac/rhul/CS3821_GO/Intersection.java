09cfdf43b49d9122c30959ecfc95645f9e47b0cd
Jai Gautam <jaigautam1405@gmail.com>
2021-10-18 04:05:57 +0100

    Created methods for setting/checking state of Intersection

--- src/main/java/uk/ac/rhul/CS3821_GO/Intersection.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/Intersection.java
@@ -3,7 +3,31 @@ package uk.ac.rhul.CS3821_GO;
 public class Intersection {
     private StoneTypes stone;
 
-    Intersection(){
+    Intersection() {
         stone = StoneTypes.NONE;
     }
+
+    public void setBlack(){
+        this.stone = StoneTypes.BLACK;
+    }
+
+    public boolean isBlack(){
+        return this.stone == StoneTypes.BLACK;
+    }
+
+    public void setWhite(){
+        this.stone = StoneTypes.WHITE;
+    }
+
+    public boolean isWhite(){
+        return this.stone == StoneTypes.WHITE;
+    }
+    public void clear(){
+        this.stone = StoneTypes.NONE;
+    }
+
+    public boolean isCleared(){
+        return this.stone == StoneTypes.NONE;
+    }
+
 }
