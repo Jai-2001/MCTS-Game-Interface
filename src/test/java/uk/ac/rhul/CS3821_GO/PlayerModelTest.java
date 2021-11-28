d6297b9ecf1cf26fa629b3319f8a72c061140e81
Jai Gautam <jaigautam1405@gmail.com>
2021-11-28 04:49:49 +0000

    created test case for combining (ideally adjacent) strings

--- src/test/java/uk/ac/rhul/CS3821_GO/PlayerModelTest.java
+++ src/test/java/uk/ac/rhul/CS3821_GO/PlayerModelTest.java
@@ -1,9 +1,17 @@
 package uk.ac.rhul.CS3821_GO;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerModelTest {
+
+    PlayerModel single;
+
+    @BeforeEach
+    void setUp() {
+        single = new PlayerModel(StoneTypes.BLACK);
+    }
 
     @Test
     void testValidInit() {
@@ -30,7 +38,6 @@ class PlayerModelTest {
 
     @Test
     void testStoneNotPresent(){
-        PlayerModel single = new PlayerModel(StoneTypes.BLACK);
         Intersection notPresent = new Intersection();
         assertThrows(IllegalArgumentException.class,
                 ()-> single.getGroup(notPresent)
@@ -39,7 +46,6 @@ class PlayerModelTest {
 
     @Test
     void testAddStone(){
-        PlayerModel single = new PlayerModel(StoneTypes.BLACK);
         Intersection unique = new Intersection();
         single.addStone(0, unique);
         assertEquals(0, single.getGroup(unique),
@@ -48,7 +54,6 @@ class PlayerModelTest {
 
     @Test
     void testDifferentStones(){
-        PlayerModel single = new PlayerModel(StoneTypes.BLACK);
         Intersection belongsToZero = new Intersection();
         Intersection belongsToOne = new Intersection();
         single.addStone(0, belongsToZero);
@@ -57,5 +62,19 @@ class PlayerModelTest {
                 "Should be able to identify corresponding string for owned stones.");
         assertEquals(1, single.getGroup(belongsToOne),
                 "Should be able to differentiate between strings");
+    }
+
+    @Test
+    void testCombineStrings(){
+        Intersection belongsToZero = new Intersection();
+        Intersection oneOrZero = new Intersection();
+        Intersection willSeeAll = new Intersection();
+        single.addStone(0, belongsToZero);
+        single.addStone(1, oneOrZero);
+        single.addStone(2, willSeeAll);
+        single.combineStrings(1,2);
+        single.combineStrings(0,1);
+        assertEquals(0, single.getGroup(oneOrZero));
+        assertEquals(0, single.getGroup(willSeeAll));
     }
 }
