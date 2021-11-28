4ade8f7b464759c295d94e2d495c48e57f2d67b4
Jai Gautam <jaigautam1405@gmail.com>
2021-11-28 04:14:04 +0000

    Refactored classes to eliminate irritating IDE warnings

--- src/main/java/uk/ac/rhul/CS3821_GO/GoModel.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/GoModel.java
@@ -5,15 +5,13 @@ public class GoModel {
     final static int BOARD_SIZE_X = 9;
     final static int BOARD_SIZE_Y = 9;
 
-    private TurnState currentPlayerTurn;
-    private StoneMap board;
-    private int[] attemptedMove;
+    private final TurnState currentPlayerTurn;
+    private final StoneMap board;
     boolean moveWasValid;
 
     GoModel(){
         this.currentPlayerTurn = new TurnState();
         this.board = new StoneMap(BOARD_SIZE_X, BOARD_SIZE_Y);
-        attemptedMove = new int[2];
         moveWasValid = false;
     }
 
@@ -22,8 +20,6 @@ public class GoModel {
     }
 
     public void nextTurn() {
-        Intersection relevant = this.board.getStone(attemptedMove[0],attemptedMove[1]);
-        PlayerModel currentPlayer = this.currentPlayerTurn.getCurrentPlayer();
         if (moveWasValid){
             this.board.confirmMove();
             this.currentPlayerTurn.changePlayer();
@@ -36,8 +32,6 @@ public class GoModel {
     }
 
     public boolean tryMove(int xPos, int yPos){
-        attemptedMove[0] = xPos;
-        attemptedMove[1] = yPos;
             if (this.board.checkMove(xPos, yPos, currentPlayerTurn.getCurrentPlayer())){
                 this.moveWasValid = true;
                 return true;
