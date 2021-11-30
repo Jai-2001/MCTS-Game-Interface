ab87da30264ebcd2d18355d1857dbdae21947371
Jai Gautam <jaigautam1405@gmail.com>
2021-11-30 00:12:06 +0000

    Refactored PlayerModel methods and fields for simplicity

--- src/main/java/uk/ac/rhul/CS3821_GO/PlayerModel.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/PlayerModel.java
@@ -1,54 +1,60 @@
 package uk.ac.rhul.CS3821_GO;
 import java.lang.IllegalArgumentException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PlayerModel {
     private final StoneTypes type;
-    private final Map<Intersection, Integer> strings;
+    private Map<Integer, Set<Intersection>> stones;
+    private Map<Intersection, Integer> stonesInverse;
 
     PlayerModel(StoneTypes type){
         if(type == StoneTypes.NONE){
             throw new IllegalArgumentException("Player must either use white or black stones.");
         }
         this.type = type;
-        this.strings = new HashMap<>();
+        this.stones = new HashMap<>();
+        this.stonesInverse = new HashMap<>();
     }
     public StoneTypes getType(){
         return this.type;
     }
 
-    public int getGroup(Intersection stone) {
-        if (!strings.containsKey(stone)){
+    public int getGroup(Intersection query) {
+        if (!stonesInverse.containsKey(query)){
             throw new IllegalArgumentException("This stone does not belong to this player.");
         }
-        return strings.get(stone);
+        return stonesInverse.get(query);
     }
 
     public void addStone(int i, Intersection stone) {
-        strings.put(stone, i);
+        Set<Intersection> groupSet;
+            if(!stones.containsKey(i)){
+                groupSet = new HashSet<>();
+                stones.put(i, groupSet);
+            } else{
+                groupSet = stones.get(i);
+            }
+        groupSet.add(stone);
+        stonesInverse.putIfAbsent(stone, i);
     }
 
     public void combineGroups(int parent, int child) {
-        strings.replaceAll((stone,group) -> group == child ? parent : group);
+        Set<Intersection> parentSet = stones.get(parent);
+        Set<Intersection> childSet = stones.get(child);
+        parentSet.addAll(childSet);
+        stonesInverse.replaceAll((stone, group)-> group == child ? parent : group);
     }
 
-    public void clearGroup(int toClear) {
-        Iterator<Map.Entry<Intersection, Integer>> mutable = strings.entrySet().iterator();
-        mutable.forEachRemaining((Map.Entry<Intersection, Integer> entry) -> {
-                int group = entry.getValue();
-                    if (group == toClear){
-                        entry.getKey().clear();
-                        mutable.remove();
-                    }
+    public void clearGroup(int i) {
+        Set<Intersection> clearSet = stones.remove(i);
+            for (Intersection toRemove: clearSet) {
+                toRemove.clear();;
+                stonesInverse.remove(toRemove);
             }
-        );
+        stones.remove(i);
     }
 
     public List<Intersection> getGroupStones(int i) {
-        return new ArrayList<Intersection>();
+        return new LinkedList<>();
     }
 }
