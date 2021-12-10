1ebef073e24d57c2bd502f11b58a6bbe8fe4327a
Jai Gautam <jaigautam1405@gmail.com>
2021-12-10 03:48:00 +0000

    Added field to turnState, allowing previous player's information to recalled

--- src/main/java/uk/ac/rhul/CS3821_GO/TurnState.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/TurnState.java
@@ -2,22 +2,29 @@ package uk.ac.rhul.CS3821_GO;
 
 public class TurnState {
     private PlayerModel current;
+    private PlayerModel previous;
     final static PlayerModel PLAYER_BLACK = new PlayerModel(StoneTypes.BLACK);
     final static PlayerModel PLAYER_WHITE = new PlayerModel(StoneTypes.WHITE);
 
     TurnState(){
         this.current = PLAYER_BLACK;
+        this.previous = PLAYER_WHITE;
     }
 
     public PlayerModel getCurrentPlayer() {
         return current;
     }
+    public PlayerModel getPreviousPlayer(){
+        return previous;
+    }
 
     public void changePlayer() {
-            if(getCurrentPlayer()== PLAYER_WHITE){
+            if(getCurrentPlayer().equals(PLAYER_WHITE)){
                 this.current = PLAYER_BLACK;
+                this.previous = PLAYER_WHITE;
             } else {
                 this.current = PLAYER_WHITE;
+                this.previous = PLAYER_BLACK;
             }
     }
 }
