d3397809da5e6c2e38a69abfff9a7e45a5023e4e
Jai Gautam <jaigautam1405@gmail.com>
2022-02-21 04:27:01 +0000

    Added main method for CMD play

--- src/main/java/uk/ac/rhul/CS3821_GO/OnePlayerManager.java
+++ src/main/java/uk/ac/rhul/CS3821_GO/OnePlayerManager.java
@@ -1,11 +1,31 @@
 package uk.ac.rhul.CS3821_GO;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class OnePlayerManager extends GoViewController {
 
     private Random rng;
     private int scoreLimit;
+
+
+    public static void main(String[] args) {
+        int scoreLimit = 5;
+        boolean isBlack = true;
+        if (args.length == 2){
+            scoreLimit = Integer.parseInt(args[0]);
+            isBlack = Integer.parseInt(args[0]) == 1;
+        }
+        OnePlayerManager manager = new OnePlayerManager(scoreLimit, isBlack);
+        Scanner inputBuffer = new Scanner(System.in);
+        do{
+            manager.inputMove(inputBuffer);
+            manager.updateBoardState();
+            manager.play();
+            manager.updateBoardState();
+        } while (!manager.someoneWon());
+        manager.updateBoardState();
+    }
 
     public OnePlayerManager(int scoreLimit, boolean isBlack){
         super();
