9d626d974550c04330a37317851f74c475a3c88e
Jai Gautam <jaigautam1405@gmail.com>
2022-02-21 23:34:35 +0000

    Added test for a tree node preparing for MCTS implementation

---
+++ src/test/java/uk/ac/rhul/CS3821_GO/GoNodeTest.java
@@ -0,0 +1,34 @@
+package uk.ac.rhul.CS3821_GO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoNodeTest {
+
+    @BeforeEach
+    void setUp() {
+        GoNode root = new GoNode();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testTraversal(){
+        GoNode childOne = new GoNode();
+        GoNode childTwo = new GoNode();
+        root.add(childOne);
+        root.add(childTwo);
+        ArrayList<GoNode> children = root.getChildren();
+        assertTrue(children.contains(childOne));
+        assertTrue(children.contains(childTwo));
+    }
+
+
+}
